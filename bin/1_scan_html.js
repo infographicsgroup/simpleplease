8d580334efce6c1dd8785bd44cac388b9ec729b7
Michael Kreil <github@michael-kreil.de>
2018-08-03 19:10:47 +0200

    work with big html too

--- bin/1_scan_html.js
+++ bin/1_scan_html.js
@@ -5,12 +5,23 @@ const fs = require('fs');
 var html = fs.readFileSync('all.html', 'utf8');
 
 console.log('replace');
-html = html.replace(/\s+/g, ' ');
+var buffer = Buffer.from(html, 'utf16le');
+if (buffer.length !== html.length*2) throw Error();
+for (var i = 0; i < buffer.length; i += 2) {
+	var value = buffer.readUInt16LE(i);
+	if (value <= 32) buffer.writeUInt16LE(32, i)
+}
+html = buffer.toString('utf16le');
+buffer = undefined;
 
 console.log('match');
 html = html.match(/\<p.*?\<\/p\>/gi);
 
-html = html.map(l => l.replace(/\<.*?\>/g, ''));
+html = html.map(l => {
+	l = l.replace(/\s+/g, ' ');
+	l = l.trim();
+	l = l.replace(/\<.*?\>/g, '')
+	return l;
+});
 
-fs.writeFileSync('text.txt', html.join('\n'), 'utf8');
-//console.log(html);
+fs.writeFileSync('all.txt', html.join('\n'), 'utf8');
